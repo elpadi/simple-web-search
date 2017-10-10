6d0ba09ef19f0b83469e2e637e400e351655b9dd
carlos (minimal) <padi_05@yahoo.com>
2017-10-10 19:45:48 -0400

    Testimonial is not part of page content

--- src/Records/RootBasics.php
+++ src/Records/RootBasics.php
@@ -1,6 +1,7 @@
 <?php
 namespace SimpleSearch\Records;
 
+use Functional as F;
 use SimpleSearch\Record;
 
 class RootBasics extends Record {
@@ -8,10 +9,11 @@ class RootBasics extends Record {
 	protected function fetchContent(\DomDocument $doc) {
 		$page = $doc->getElementById('page');
 		if ($page) {
-			foreach ($page->childNodes as $node)
-				if (in_array($node->nodeName, ['HEADER','FOOTER']))
-					$page->removeChild($node);
-			$this->content = $doc->saveHtml($page);
+			$this->content = implode(' ', F\map(F\filter(iterator_to_array($page->childNodes), function($node) {
+				return !in_array($node->nodeName, ['#text','header','footer']);
+			}), function($node) use ($doc) {
+				return $doc->saveHtml($node);
+			}));
 		}
 		else {
 			$content = $doc->getElementById('content');
